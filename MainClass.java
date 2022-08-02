63cc3bee9b291f55fb080daa89954575659808fa
Reemamaiman <110451179+Reemamaiman@users.noreply.github.com>
2022-08-02 17:18:55 +0300

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,20 @@
+package com.company;
+
+
+
+public class MainClass {
+
+
+    public static int[] makeArray(int num1, int num2, int num3){
+        int[]arr={num1,num2,num3};
+        return arr;
+        {
+            public static void main(String[] args) {
+                int[] myArray = makeArray(1,2,5);
+            {
+                {
+        }
+        }
+        }
+    }}
+
