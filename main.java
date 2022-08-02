844b53863a6923653ef2fe3e22e069fdaf5a443f
Reemamaiman <110451179+Reemamaiman@users.noreply.github.com>
2022-08-02 17:20:05 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,120 @@
+package com.company;
+
+public class main {
+
+    public static void main(String[]args) {
+        //Q1
+      String user[]=new String[3];
+
+
+        user = new String[]{"Saleh", "Ali", "Khalid"};
+        user[0]="Saleh";
+        user[1]="Ali";
+        user[2]="Khalid";
+
+        System.out.print("_________________________________");
+        //Q3
+
+
+
+                for (int i = 1; i <= 20; i++) {
+                    if (i % 5 != 0)
+                        System.out.println(i);
+
+                }
+
+                    System.out.print("_________________________________");
+                    //Q4
+                    String [] myArray = new String[10];
+                    myArray[0]="one";
+                    myArray[1]="two";
+                    myArray[2]="three";
+                    myArray[3]="four";
+                    myArray[4]="five";
+                    myArray[5]="Alfa";
+                    myArray[6]="Mouse";
+                    myArray[7]="Apple";
+                    myArray[8]="Reema";
+                    myArray[9]="Majd";
+                    for (int j = 0; j <10 ; j++) {
+                        {
+                            if (!myArray[i].startsWith("A"))
+                                System.out.println(myArray[i]);
+                        }
+                    }
+
+                    System.out.print("_________________________________");
+                    //Q5
+        int[] intArray = {10,20,30,40,50,60,70,80};
+        printArray(intArray);
+
+        public static void printArray(int)
+                    for (int i = 0; i < intArray.length; i++)
+                    {
+                        System.out.println(intArray[i]);
+                    }
+
+        System.out.print("_________________________________");
+        //Q6
+        public static int[] makeArray(int num1, int num2, int num3){
+            int[]arr={num1,num2,num3};
+            return arr;
+            {
+                public static void main(String[] args) {
+                int[] myArray = makeArray(1,2,5);
+                {
+
+
+        System.out.print("_________________________________");
+        //Q7
+                public class Ex7 {
+                    public static void main(String[] args) {
+                       public void fullName (String firstName, String lastName){
+                       System.out.println("Hello" + firstName + "" + lastName);
+                }}}
+
+
+                    System.out.print("_________________________________");
+                    //Q8
+                    public float Average(int num1, int num2, int num3, int num4) {
+                    float avr = (num1 + num2 + num3 + num4) / 4;
+
+                    return avr;
+                    {
+                        System.out.print("_________________________________");
+                        //Q9
+                        public int maxNumber(int[] arr) {
+
+                        int max = 0;
+                        for (int i = 0; i < arr.length; i++) {
+                            if (arr[i] > max)
+                                max = arr[i];
+                        }
+
+                        return max;
+                    }
+
+                        public static void main(String[] args) {
+
+
+                    }
+
+                    }
+
+                }
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
